2d9070c9a02372ec3210819093a960b7e907957b
zhouliang <zhouliang@xbonline.net>
2019-06-29 11:39:15 +0800

    lock-on-active

--- src/main/java/com/zl/basic/LockOnActiveT.java
+++ src/main/java/com/zl/basic/LockOnActiveT.java
@@ -3,19 +3,16 @@ package com.zl.basic;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.rule.FactHandle;
 
-public class NoLoopT {
+public class LockOnActiveT {
 
     public static void main(String[] args) {
         KieServices kieServices = KieServices.Factory.get();
         KieContainer kieContainer = kieServices.getKieClasspathContainer();
         KieSession kieSession = kieContainer.newKieSession("BasicKS");
 
-        NoLoop noLoop = new NoLoop("score", 10);
-        FactHandle factHandle = kieSession.insert(noLoop);
+        LockOnActive lockOnActive = new LockOnActive("zhouliang", 13);
+        kieSession.insert(lockOnActive);
         kieSession.fireAllRules();
-
-        kieSession.dispose();
     }
 }
